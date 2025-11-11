252c873e41e92279f1a1001dad9f255e159025c8
MEDamine1910 <rouissmohamedamine@gmail.com>
2025-11-11 22:13:12 +0100

    .

---
+++ TpREST/src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    int id;
+    String nom;
+    String mail;
+}
